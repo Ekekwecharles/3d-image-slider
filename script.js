e95b65bd62fbe09df09ad92237796755a219d729
Charlessnow <ekekwecharles50@gmail.com>
2024-07-29 07:03:28 +0100

    updates

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
-document.querySelector(".slider").style.setProperty("--rotate-x", "0");
-let rotateXValue = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".slider").style.setProperty("--rotate-x", "0");
+  let rotateXValue = 0;
 
-document.addEventListener("keydown", function (e) {
-  // console.log(e);
-  if (e.key === "ArrowUp") {
+  document.addEventListener("keydown", function (e) {
+    // console.log(e);
+    if (e.key === "ArrowUp") {
+      rotateXValue -= 1;
+    } else if (e.key === "ArrowDown") {
+      rotateXValue += 1;
+    }
+
+    document
+      .querySelector(".slider")
+      .style.setProperty("--rotate-x", `${rotateXValue}deg`);
+  });
+
+  const up = document.querySelector(".up");
+  const down = document.querySelector(".down");
+
+  up.addEventListener("click", function (e) {
+    console.log("Up button clicked");
     rotateXValue -= 1;
-  } else if (e.key === "ArrowDown") {
-    rotateXValue += 1;
-  }
 
-  document
-    .querySelector(".slider")
-    .style.setProperty("--rotate-x", `${rotateXValue}deg`);
+    document
+      .querySelector(".slider")
+      .style.setProperty("--rotate-x", `${rotateXValue}deg`);
+  });
+
+  down.addEventListener("click", function (e) {
+    console.log("down button clicked");
+    rotateXValue += 1;
+
+    document
+      .querySelector(".slider")
+      .style.setProperty("--rotate-x", `${rotateXValue}deg`);
+  });
 });
